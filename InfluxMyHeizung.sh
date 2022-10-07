403a1efd94f5a8cae320f70f724b11ceaeb4e92c
DonKingMat <DonKingMat@users.noreply.github.com>
2022-10-07 18:16:09 +0200

    GoLiveNow

--- InfluxMyHeizung.sh
+++ InfluxMyHeizung.sh
@@ -10,7 +10,7 @@ if [ "$1" = "debug" ] ; then DEBUG=1 ; else DEBUG=0 ; fi
 
 # Ich übersetze die Befehle, weil auf einem Ubuntu oder einem CentOS oder einem Raspberry-Image die Befehle an
 # unterschiedlichen Stellen liegen können. So kann ich dieses Skript in 5 oder 10 Jahren auf einer beliebigen
-# anderen Distribution einfach weiter laufen lassen
+# anderen Distribution einfach weiter laufen lassen.
 DATE=`which date`
 JQ=`which jq`
 WGET=`which wget`
@@ -22,20 +22,28 @@ NC=`which nc`
 if [ $($PIDOF -x "$0" | wc -w) -ne 2 ] ; then exit ; fi
 
 # Die INFLUX-Variablen sind für das Schreiben in die Influx >V2
-# Der ESP mit Tasmota FW wird definiert
+# Der ESP mit Tasmota Firmware wird definiert
 ESP=192.168.x.y
-NAME=Heizung
 INFLUXRASPI=192.168.x.z:8086
-INFLUXRASPIORG=*****
-INFLUXRASPIBUCKET=*****
-INFLUXRASPITOKEN=*****
+INFLUXRASPIORG=****
+INFLUXRASPIBUCKET=****
+INFLUXRASPITOKEN=****
 
-# (Fast) Stillschweigender Skript Abbruch, wenn der ESP nicht erreichbar ist
+# Skript-Abbruch, wenn der ESP nicht erreichbar ist
 # Prüfung per nc auf Port 80
 if $(nc -z -w2 ${ESP} 80) ; then
 
   # Alle JSON Informationen aus dem Tasmota auslesen und später per echo $JSON verarbeiten
   JSON=$($CURL -s ${ESP}/cm?cmnd=status%2010)
+  JSON5=$($CURL -s ${ESP}/cm?cmnd=status%205)
+  JSON11=$($CURL -s ${ESP}/cm?cmnd=status%2011)
+
+  # Wie hast du deinen Tasmota genannt? So taucht er im Influx/Grafana auf
+  ESPHOSTNAME=$(echo $JSON5 | $JQ -r '."StatusNET"."Hostname"')
+  
+  # RSSI und Signal Werte aus dem tasmota ESP Image auslesen
+  ESPRSSI=$(echo $JSON11 | $JQ -r '."StatusSTS"."Wifi"."RSSI"')
+  ESPSIGNAL=$(echo $JSON11 | $JQ -r '."StatusSTS"."Wifi"."Signal"')
 
   # Alternativ könnte man die EPOCH Uhrzeit aus dem JSON des ESP nehmen, aber das impliziert
   # ein sauberes anpassen der Zeitzonen. Also lieber das EPOCH vom Rechner nehmen, der das hier ausführt
@@ -56,22 +64,24 @@ for SENSOR in $SENSOREN ; do
 
   # Klar, kann man das schöner programmieren, aber so verstehts halt direkt jeder - Vor allem ich in 2 Jahren!
   # Die doppelte Maskierung um den $SENSOR ist, um Fehler in der Auswertung im jq zu vermeiden (Bindestrich)
-  if [ "$(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Id')" == "000000088888" ] ; then SENSORNAME="Raumluft"  ; fi
-  if [ "$(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Id')" == "000000099999" ] ; then SENSORNAME="FBH.Rücklauf" ; fi
-  if [ "$(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Id')" == "0000000AAAAA" ] ; then SENSORNAME="FBH.Vorlauf" ; fi
-  if [ "$(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Id')" == "0000000BBBBB" ] ; then SENSORNAME="Heizkörper.Vorlauf"  ; fi
-  if [ "$(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Id')" == "0000000CCCCC" ] ; then SENSORNAME="Heizkörper.Rücklauf"  ; fi
-  if [ "$(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Id')" == "0000000DDDDD" ] ; then SENSORNAME="HeißwasserZirkulation" ; fi
-  if [ "$(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Id')" == "0000000EEEEE" ] ; then SENSORNAME="Heißwasser" ; fi
+  # Ersetze hier die "000000000000" mit den IDs deiner Sensoren und je SENSORNAME, was der Sensor misst.
+  if [ "$(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Id')" == "000000000000" ] ; then SENSORNAME="Heizungskeller"  ; fi
+  if [ "$(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Id')" == "000000000000" ] ; then SENSORNAME="FBH_Rücklauf" ; fi
+  if [ "$(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Id')" == "000000000000" ] ; then SENSORNAME="FBH_Vorlauf" ; fi
+  if [ "$(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Id')" == "000000000000" ] ; then SENSORNAME="Heizkörper_Vorlauf"  ; fi
+  if [ "$(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Id')" == "000000000000" ] ; then SENSORNAME="Heizkörper_Rücklauf"  ; fi
+  if [ "$(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Id')" == "000000000000" ] ; then SENSORNAME="Zirkulation" ; fi
+  if [ "$(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Id')" == "000000000000" ] ; then SENSORNAME="Heißwasser" ; fi
   if [ "$SENSORNAME" != "null" ] ; then
+
     TEMPERATUR=$(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Temperature')
     # Debug Ausgabe, wenn der gefundene Sensor in diesem Loop ist
     if [ $DEBUG -eq 1 ] ; then
-      echo "# --- Sensor $SENSORNAME hat $TEMPERATUR °C und den Namen $SENSOR und die ID $(echo $JSON | $JQ -r '.StatusSNS."'$SENSOR'".Id')"
+      echo "# --- Es sind $TEMPERATUR °C an $SENSORNAME"
     fi
 
     # Schreiben des Temperaturwertes in die InfluxDB
-    $CURL --request POST "http://${INFLUXRASPI}/api/v2/write?org=${INFLUXRASPIORG}&bucket=${INFLUXRASPIBUCKET}&precision=s" --header "Authorization: Token ${INFLUXRASPITOKEN}" --data-raw "${NAME}.${SENSORNAME} Temperatur=${TEMPERATUR} ${EPOCH}"
+    $CURL --request POST "http://${INFLUXRASPI}/api/v2/write?org=${INFLUXRASPIORG}&bucket=${INFLUXRASPIBUCKET}&precision=s" --header "Authorization: Token ${INFLUXRASPITOKEN}" --data-raw "${ESPHOSTNAME} ${SENSORNAME}=${TEMPERATUR} ${EPOCH}"
 
   else
 
@@ -83,4 +93,11 @@ for SENSOR in $SENSOREN ; do
   fi
 done
 
-exit
+# Falls man das alles mal auf der CLI sehen möchte
+if [ $DEBUG -eq 1 ] ; then echo "# --- Sensor $ESPHOSTNAME RSSI: $ESPRSSI Signal: $ESPSIGNAL" ; fi
+
+# RSSI und Signal Werte aus dem tasmota ESP Image speichern
+$CURL --request POST "http://${INFLUXRASPI}/api/v2/write?org=${INFLUXRASPIORG}&bucket=${INFLUXRASPIBUCKET}&precision=s" --header "Authorization: Token ${INFLUXRASPITOKEN}" --data-raw "${ESPHOSTNAME} RSSI=${ESPRSSI} ${EPOCH}"
+$CURL --request POST "http://${INFLUXRASPI}/api/v2/write?org=${INFLUXRASPIORG}&bucket=${INFLUXRASPIBUCKET}&precision=s" --header "Authorization: Token ${INFLUXRASPITOKEN}" --data-raw "${ESPHOSTNAME} Signal=${ESPSIGNAL} ${EPOCH}"
+
+exit 0
